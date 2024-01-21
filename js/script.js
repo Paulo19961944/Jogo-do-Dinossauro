e0d9c7bb29d1e26198abde483be3412c88c2c8a2
Paulo19961944 <paulo24041996@gmail.com>
2024-01-21 16:48:26 -0300

    Jogo do Dinossauro V1.0.1

---
+++ js/script.js
@@ -0,0 +1,52 @@
+const dino = document.querySelector('.dino-game');
+const rock = document.querySelector('.rock');
+let gameover = false;
+let score = 0;
+
+const updateScore = () => {
+    const scoreElement = document.getElementById('scoreValue');
+    score += 10;
+    scoreElement.textContent = score;
+};
+
+const jump = () => {
+    if (!gameover && !dino.classList.contains('jump')) {
+        dino.classList.add('jump');
+
+        setTimeout(() => {
+            dino.classList.remove('jump');
+        }, 1000);
+
+        updateScore(); // Aumenta a pontuação apenas uma vez por salto
+    }
+};
+
+const restartGame = () => {
+    gameover = false;
+    score = 0;
+    document.getElementById('scoreValue').textContent = score;
+    // Reinicialize outros estados ou elementos, se necessário
+};
+
+const checkCollision = () => {
+    const dinoBottom = parseInt(getComputedStyle(dino).getPropertyValue('bottom'));
+    const rockLeft = parseInt(getComputedStyle(rock).getPropertyValue('left'));
+
+    if (dinoBottom < 9 && rockLeft > 0 && rockLeft < 50) {
+        gameover = true;
+        alert('Game Over! A sua pontuação é: ' + score);
+        restartGame();
+    }
+
+    if (!gameover) {
+        requestAnimationFrame(checkCollision);
+    }
+};
+
+document.addEventListener('keydown', (event) => {
+    if (event.keyCode === 32) { // Barra de espaço
+        jump();
+    }
+});
+
+checkCollision(); // Inicia a verificação contínua da colisão
