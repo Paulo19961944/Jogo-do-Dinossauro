3eaa0bb6c6a0070074e61fcf2d625f9e8974aafc
Paulo19961944 <paulo24041996@gmail.com>
2024-05-19 20:06:59 -0300

    [Fix] Resolvendo Bugs do Obstáculo

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 const dino = document.querySelector('.dino-game'); // Captura o Dinossauro
-const rock = document.querySelector('.rock'); // Captura a Pedra
+const obstaculo = document.querySelector('.obstaculo'); // Captura a Pedra
 const containerGame = document.querySelector('.game-container'); // Captura o Container do Jogo
 const windowWidth = window.innerWidth; // Captura a Largura da Janela
 let gameover = false; // Inicia o jogo direto
@@ -18,7 +18,7 @@ const jump = () => {
         dino.classList.add('jump'); // Adiciona a classe jump
         setTimeout(() => {
             dino.classList.remove('jump'); // Remove a classe jump
-        }, 2000);
+        }, 1500);
         updateScore(); // Atualiza a pontuação quando o dinossauro pular
     }
 };
@@ -33,29 +33,29 @@ const restartGame = () => {
 // Verifica se Há Colisões
 const checkCollision = () => {
     const dinoBottom = parseInt(getComputedStyle(dino).getPropertyValue('bottom')); // Captura a posição do dinossauro
-    const rockRight = parseInt(getComputedStyle(rock).getPropertyValue('right')); // Captura a posição da pedra
+    const obstaculoRight = parseInt(getComputedStyle(obstaculo).getPropertyValue('right')); // Captura a posição da pedra
     const containerWidth = containerGame.offsetWidth; // Obtém a largura do containerGame em pixels
-    let rockRightInitialAbs, rockRightFinalAbs;
+    let obstaculoRightInitialAbs, obstaculoRightFinalAbs;
 
     if (windowWidth < 480) {
-        rockRightInitialAbs = containerWidth * 0.54; // 54% da largura do elemento pai
-        rockRightFinalAbs = containerWidth * 0.79; // 79% da largura do elemento pai
+        obstaculoRightInitialAbs = containerWidth * 0.55; // 55% da largura do elemento pai
+        obstaculoRightFinalAbs = containerWidth * 0.72; // 72% da largura do elemento pai
     } else if (windowWidth >= 480 && windowWidth < 768) {
-        rockRightInitialAbs = containerWidth * 0.72; // 72% da largura do elemento pai
-        rockRightFinalAbs = containerWidth * 0.86; // 86% da largura do elemento pai
+        obstaculoRightInitialAbs = containerWidth * 0.71; // 71% da largura do elemento pai
+        obstaculoRightFinalAbs = containerWidth * 0.84; // 84% da largura do elemento pai
     } else if (windowWidth >= 768 && windowWidth < 1440) {
-        rockRightInitialAbs = containerWidth * 0.65; // 65% da largura do elemento pai
-        rockRightFinalAbs = containerWidth * 0.84; // 84% da largura do elemento pai
+        obstaculoRightInitialAbs = containerWidth * 0.68; // 68% da largura do elemento pai
+        obstaculoRightFinalAbs = containerWidth * 0.82; // 82% da largura do elemento pai
     } else if (windowWidth >= 1440 && windowWidth < 1920) {
-        rockRightInitialAbs = containerWidth * 0.80; // 80% da largura do elemento pai
-        rockRightFinalAbs = containerWidth * 0.91; // 91% da largura do elemento pai
+        obstaculoRightInitialAbs = containerWidth * 0.81; // 81% da largura do elemento pai
+        obstaculoRightFinalAbs = containerWidth * 0.90; // 90% da largura do elemento pai
     } else {
-        rockRightInitialAbs = containerWidth * 0.65; // 65% da largura do elemento pai
-        rockRightFinalAbs = containerWidth * 0.84; // 84% da largura do elemento pai
+        obstaculoRightInitialAbs = containerWidth * 0.67; // 67% da largura do elemento pai
+        obstaculoRightFinalAbs = containerWidth * 0.82; // 82% da largura do elemento pai
     }
 
     // Verificando as condições de colisão
-    if (dinoBottom <= -32 && rockRight > rockRightInitialAbs && rockRight < rockRightFinalAbs) {
+    if (dinoBottom <= -2 && obstaculoRight > obstaculoRightInitialAbs && obstaculoRight < obstaculoRightFinalAbs) {
         gameover = true; // Game Over
         alert('Game Over! A sua pontuação é: ' + score); // Mostra a pontuação na tela
         restartGame(); // Inicializa o jogo novamente
